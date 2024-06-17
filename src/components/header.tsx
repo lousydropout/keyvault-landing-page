bd8f39745d59a3551f76ff808872e0d72832fcdf
lousydropout <lousydropout@gmail.com>
2024-06-17 12:53:25 -0700

    add usePathname

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,17 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
 export default function Headers() {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/docs", name: "Documentation" },
+    { href: "/pricing", name: "Pricing" },
+    { href: "/about", name: "About" },
+  ];
+
   return (
     <div className="flex flex-wrap gap-8 py-4 justify-between items-center">
       <div className="flex">
@@ -9,14 +21,19 @@ export default function Headers() {
         <p className=" italic text-accent">beta</p>
       </div>
       <nav className="flex gap-2 sm:gap-4 md:gap-8 text-xl">
-        <Link className="hidden md:inline-block" href="/docs">
-          Documentation
-        </Link>
-        <Link className="md:hidden" href="/docs">
-          Docs
-        </Link>
-        <Link href="/pricing">Pricing</Link>
-        <Link href="/about">About</Link>
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx({
+                "text-accent underline": pathname === link.href,
+              })}
+            >
+              <p>{link.name}</p>
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
